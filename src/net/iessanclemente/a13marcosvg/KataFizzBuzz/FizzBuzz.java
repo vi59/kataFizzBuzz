9f6720cb31bbd2b81f2e9f31ff75603e5c9d2fe5
dawMP - Viqueira García <A13MarcosVG@sanclemente.local>
2016-05-10 20:26:23 +0200

    Agregado metodo para comprobar que un numero es devuelto como cadena

--- src/net/iessanclemente/a13marcosvg/KataFizzBuzz/FizzBuzz.java
+++ src/net/iessanclemente/a13marcosvg/KataFizzBuzz/FizzBuzz.java
@@ -1,7 +1,12 @@
 package net.iessanclemente.a13marcosvg.KataFizzBuzz;
 
 public class FizzBuzz {
+	/**
+	 * Metodo evaluate() que convierte un numero entero en una cadena 
+	 * @param number
+	 * @return devuleve el numero convertido a cadena
+	 */
 	public String evaluate(int number){
-		return null;
+		return String.valueOf(number);
 	}
 }
